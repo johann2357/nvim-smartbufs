f1c48e166a593056f31d439779a233bc9dfbe072
Johann Nunez Garcia <johann2357@gmail.com>
2021-03-30 00:48:18 -0500

    fix(deprecation): support old function names

--- lua/nvim-smartbufs/init.lua
+++ lua/nvim-smartbufs/init.lua
@@ -4,10 +4,16 @@ local buff = require("nvim-smartbufs.buff")
 local M = {}
 
 
+-- Stable
 M.goto_terminal = term.goto_terminal
 M.goto_buffer = buff.goto_buffer
 M.goto_next_buffer = buff.goto_next_buffer
 M.goto_prev_buffer = buff.goto_prev_buffer
+
+-- Deprecated
+M.open_n_buffer = buff.goto_buffer
+M.open_next_buffer = buff.goto_next_buffer
+M.open_prev_buffer = buff.goto_prev_buffer
 
 
 return M
